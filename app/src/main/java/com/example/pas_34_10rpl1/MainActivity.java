c4ad9bc79b20b9c3aff1b0aa8082d6c003db606a
Umar Setyawan <umarsetyawan10@gmail.com>
2021-06-03 10:29:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pas_34_10rpl1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pas_34_10rpl1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
